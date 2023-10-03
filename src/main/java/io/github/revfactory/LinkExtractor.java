87b2a86ce2192f53e7f73f45578f9998af0d04e4
robin-hwang <robin.hwang@kakaocorp.com>
2023-10-04 01:16:01 +0900

    Ensure that only lower url is extracted

--- src/main/java/io/github/revfactory/LinkExtractor.java
+++ src/main/java/io/github/revfactory/LinkExtractor.java
@@ -5,18 +5,25 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LinkExtractor {
-    public static List<String> extractLinks(String url) throws IOException {
+    public static List<String> extractLinks(String sourceUrl) throws IOException {
         List<String> links = new ArrayList<>();
-        Document document = Jsoup.connect(url).get();
+
+        URL url = new URL(sourceUrl);
+        String base = url.getProtocol() + "://" + url.getHost();
+        String path = url.getPath();
+
+        Document document = Jsoup.connect(sourceUrl).get();
         Elements elements = document.select("a[href]");
 
         for (var element : elements) {
             String link = element.attr("abs:href");
-            if (!link.isBlank()) {
+
+            if (link.startsWith(base + path)) {
                 links.add(link);
             }
         }
